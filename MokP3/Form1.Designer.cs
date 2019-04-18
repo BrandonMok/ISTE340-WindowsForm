3d7a0b2d3e42d59748e350da502058ed5b8057ee
brandonmok <brandonmok88@yahoo.com>
2019-04-17 20:12:33 -0400

    First commit

---
+++ MokP3/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MokP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_quote = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.menu_strip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.degreesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.employmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.peopleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.researchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resourcesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facultyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_strip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_quote
+            // 
+            this.lbl_quote.AutoSize = true;
+            this.lbl_quote.Location = new System.Drawing.Point(174, 279);
+            this.lbl_quote.Name = "lbl_quote";
+            this.lbl_quote.Size = new System.Drawing.Size(51, 20);
+            this.lbl_quote.TabIndex = 0;
+            this.lbl_quote.Text = "label2";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(174, 393);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(80, 20);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Location = new System.Drawing.Point(174, 55);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(51, 20);
+            this.lbl_description.TabIndex = 3;
+            this.lbl_description.Text = "label1";
+            // 
+            // menu_strip
+            // 
+            this.menu_strip.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.menu_strip.AllowDrop = true;
+            this.menu_strip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menu_strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.degreesToolStripMenuItem,
+            this.minorsToolStripMenuItem,
+            this.employmentToolStripMenuItem,
+            this.peopleToolStripMenuItem,
+            this.researchToolStripMenuItem,
+            this.resourcesToolStripMenuItem});
+            this.menu_strip.Name = "menu_strip";
+            this.menu_strip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menu_strip.ShowCheckMargin = true;
+            this.menu_strip.Size = new System.Drawing.Size(208, 214);
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // degreesToolStripMenuItem
+            // 
+            this.degreesToolStripMenuItem.Name = "degreesToolStripMenuItem";
+            this.degreesToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.degreesToolStripMenuItem.Text = "Degrees";
+            // 
+            // minorsToolStripMenuItem
+            // 
+            this.minorsToolStripMenuItem.Name = "minorsToolStripMenuItem";
+            this.minorsToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.minorsToolStripMenuItem.Text = "Minors";
+            // 
+            // employmentToolStripMenuItem
+            // 
+            this.employmentToolStripMenuItem.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.employmentToolStripMenuItem.Name = "employmentToolStripMenuItem";
+            this.employmentToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.employmentToolStripMenuItem.Text = "Employment";
+            // 
+            // peopleToolStripMenuItem
+            // 
+            this.peopleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facultyToolStripMenuItem,
+            this.staffToolStripMenuItem});
+            this.peopleToolStripMenuItem.Name = "peopleToolStripMenuItem";
+            this.peopleToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.peopleToolStripMenuItem.Text = "People";
+            // 
+            // researchToolStripMenuItem
+            // 
+            this.researchToolStripMenuItem.Name = "researchToolStripMenuItem";
+            this.researchToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.researchToolStripMenuItem.Text = "Research";
+            // 
+            // resourcesToolStripMenuItem
+            // 
+            this.resourcesToolStripMenuItem.Name = "resourcesToolStripMenuItem";
+            this.resourcesToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.resourcesToolStripMenuItem.Text = "Resources";
+            // 
+            // facultyToolStripMenuItem
+            // 
+            this.facultyToolStripMenuItem.Name = "facultyToolStripMenuItem";
+            this.facultyToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.facultyToolStripMenuItem.Text = "Faculty";
+            // 
+            // staffToolStripMenuItem
+            // 
+            this.staffToolStripMenuItem.Name = "staffToolStripMenuItem";
+            this.staffToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.staffToolStripMenuItem.Text = "Staff";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 510);
+            this.Controls.Add(this.lbl_description);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lbl_quote);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu_strip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_quote;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.ContextMenuStrip menu_strip;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem degreesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem employmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem peopleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facultyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem researchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resourcesToolStripMenuItem;
+    }
+}
+
---
+++ MokP3/Form1.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MokP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Change size of the form!
+            this.Size = new Size(700,450);
+
+            // json2csharp.com
+
+
+            // ABOUT
+            string jsonAbout = getRESTData("/about/"); 
+            About about = JToken.Parse(jsonAbout).ToObject<About>(); // cast it to the About object
+
+            //textBox1.Text = about.description;
+            lbl_description.Text = about.description;
+            lbl_description.MaximumSize = new Size(500, 0);
+            lbl_description.Font = new Font("Arial", 9);
+
+            lbl_quote.Text = about.quote;
+            lbl_quote.MaximumSize = new Size(400, 0);
+            lbl_quote.Font = new Font("Arial", 8);
+
+
+            // DEGREES
+            string jsonDegrees = getRESTData("/degrees/");
+            Degrees degrees = JToken.Parse(jsonDegrees).ToObject<Degrees>();
+
+
+
+            // MINORS
+            string jsonMinors = getRESTData("/minors/");
+            Minors minors = JToken.Parse(jsonMinors).ToObject<Minors>();
+
+
+            // EMPLOYMENT
+            string jsonEmployment = getRESTData("/employment/");
+            Employment employment = JToken.Parse(jsonEmployment).ToObject<Employment>();
+
+
+            // RESEARCH
+            string jsonResearch = getRESTData("/research/");
+            Research research = JToken.Parse(jsonResearch).ToObject<Research>();
+
+
+            // NEWS
+            string jsonNews = getRESTData("/news/");
+            News news = JToken.Parse(jsonNews).ToObject<News>();
+
+
+            // FOOTER
+            string jsonFooter = getRESTData("/footer/");
+            Footer footer = JToken.Parse(jsonFooter).ToObject<Footer>();
+
+
+
+
+            // PEOPLE
+            string jsonPeople = getRESTData("/people/");
+            People people = JToken.Parse(jsonPeople).ToObject<People>();
+            //Console.WriteLine(jsonPeople);
+            Console.WriteLine(people.faculty); // returns a list
+
+            //for(int i = 0; i < people.faculty.Count; i++)
+            //{
+            //    Console.WriteLine(people.faculty[i].username);
+            //}
+
+
+
+            // RESOURCES
+            string jsonResources = getRESTData("/resources/");
+            Resources resources = JToken.Parse(jsonResources).ToObject<Resources>();
+            //populate the link label with the RIT JobZoneLink!
+            linkLabel1.Text = resources.coopEnrollment.RITJobZoneGuidelink;
+        }
+
+
+
+
+        #region getRestData
+
+        public string getRESTData(string uri)
+        {
+            // base URI
+            string baseUri = "http://ist.rit.edu/api";
+
+            // connect to the api
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseUri + uri);
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException we)
+            {
+                WebResponse err = we.Response;
+                using(Stream responseStream = err.GetResponseStream())
+                {
+                    StreamReader r = new StreamReader(responseStream, Encoding.UTF8);
+                    return r.ReadToEnd();
+                }
+            }
+        }
+
+        #endregion
+
+
+
+        // Can be used multiple times
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // go and load the pdf!
+            // which url do I need? We can get it from sender
+
+            // LinkLabel me = (LinkLabel)sender; 
+            LinkLabel me = sender as LinkLabel;
+
+            // make me look like I was visited
+            me.LinkVisited = true;
+
+            System.Diagnostics.Process.Start(me.Text);
+        }
+    }
+}
